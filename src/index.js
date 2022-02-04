1a84eb913545ade60eacbde3b622c9f18a4bd888
Kevin Alberto Palma Ralda <kapalma05@gmail.com>
2022-02-04 16:22:31 -0600

    :sparkles: Change Team
    Se introdujo un boton para cambiar el equipo que sale aleatoreamente para que si el usuario no esta contento pruebe suerte luego de esto..!

--- src/index.js
+++ src/index.js
@@ -4,9 +4,18 @@ import './styles/main.css'
 // Template().then((value) => {
 //     console.log(value)
 // })
+const container = null || document.querySelector('.poke-container');
 
-(async function App() {
-    const container = null || document.querySelector('.poke-container');
+const getTeam = document.querySelector('.getData').addEventListener('click', () => {
+    DrawTemplate()
+})
+const DrawTemplate = async () => {
     let object = await Template()
+    if (container.childElementCount > 0) {
+        let cards = document.querySelectorAll('.poke-card')
+        cards.forEach(card => {
+            card.remove()
+        });
+    }
     container.append(...object)
-})();
+};
